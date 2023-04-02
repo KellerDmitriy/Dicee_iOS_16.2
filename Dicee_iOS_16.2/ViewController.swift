c9f9ec09608839612fe762f39d307d600e85f809
DmKeller <dmit.keller@gmail.com>
2023-04-02 11:03:28 +0300

    Initial Commit

---
+++ Dicee_iOS_16.2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Dicee_iOS_16.2
+//
+//  Created by Келлер Дмитрий on 02.04.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
